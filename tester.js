1f3fe41fb498c627d23d0d470becdc3e8bd2373d
smirnovdv <volmondo@gmail.com>
2020-03-01 22:42:03 -0600

    first commit

--- tester.js
+++ tester.js
@@ -5,10 +5,12 @@ async function scrape(url) {
     const browser = await puppeeter.launch();
     const page = await browser.newPage();
     await page.goto(url);
-    for (let i = 1; i<100; i++) {
+    const rowsAvailable = await page.$eval(`#current_holdings_table > tbody`,e => e.childNodes.length)
+    for (let i = 1; i<rowsAvailable; i++) {
         const name = await page.$eval(`#current_holdings_table > tbody > tr:nth-child(${i}) > td:nth-child(1)`,e => e.innerText)
-        const value = await page.$eval(`#current_holdings_table > tbody > tr:nth-child(${i}) > td:nth-child(6)`,e => e.innerText)
-        console.log(name + ' ' + value)
+        const share = await page.$eval(`#current_holdings_table > tbody > tr:nth-child(${i}) > td:nth-child(6)`,e => e.innerText)
+        const change = await page.$eval(`#current_holdings_table > tbody > tr:nth-child(${i}) > td:nth-child(10)`,e => e.innerText)
+        console.log(name + ' ' + share + ' ' + change)
     }
 
 
